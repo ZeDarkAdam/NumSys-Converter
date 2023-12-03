62c6e9c393be57fd5abb688bd0c705655d181ca4
Oleh <80891624+ZeDarkAdam@users.noreply.github.com>
2023-12-03 23:55:13 +0200

    Update NumSys_Converter.py

--- NumSys_Converter.py
+++ NumSys_Converter.py
@@ -116,6 +116,8 @@ if number_to_convert is not None:
         if is_from_base(number_to_convert, from_base):
             code = convert(number_to_convert)
             st.code(code)
+            if "number" not in st.session_state:
+                st.session_state['number'] = number_to_convert
         else:
             st.error("Невірний ввід")
 else:
